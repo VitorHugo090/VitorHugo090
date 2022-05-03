1260e74a23d9085cc8e11eb554a0c74dfce3a4b1
VITOR HUGO FERNANDES DA SILVA <VITOR_H@tcm.go>
2022-05-03 14:53:23 -0300

    Adicionando o arquivo

---
+++ src/main/java/br/com/tcmgo/tcmgomaven/App.java
@@ -0,0 +1,22 @@
+package br.com.tcmgo.tcmgomaven;
+
+public class App {
+    public static void main( String[] args ) {
+     //   String url = "https://viacep.com.br/ws/" + args[1] + "/json/";
+
+        System.out.println("Hello World");
+
+
+
+      /* try {
+            CloseableHttpClient client = HttpClientBuilder.create().build();
+            CloseableHttpResponse response = client.execute(new HttpGet(url));
+            String json = EntityUtils.toString(response.getEntity());
+
+            System.out.println(json);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+*/
+    }
+}
